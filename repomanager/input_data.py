c29bdb13aa3190497301e9bf877042de89338d29
albaadani1996 <gusalbkh@student.gu.se>
2024-05-09 17:26:15 +0200

    update input data

--- repomanager/input_data.py
+++ repomanager/input_data.py
@@ -1,170 +1,93 @@
 import ast
-from ast import *
 import difflib
-import os
-import re
-import shutil
-from git import Repo, InvalidGitRepositoryError
-from urllib.parse import urlparse
+import pandas as pd
 from repo_utils import clone_repo
 from repo_changes import commit_changes
-import pandas as pd
-import astor
 
-def get_file_content(repo, commit, file_path):
-    try:
-        return commit.tree[file_path].data_stream.read().decode('utf-8')
-    except KeyError:
-        return None
+# Updated input_functions with detailed tracking of library functions
+input_functions = {
+    'cv2': ['imread', 'VideoCapture', 'imdecode'], # Opencv, used in image and video analysis applications
+    'np': ['load', 'loadtxt', 'fromstring', 'frombuffer', 'genfromtxt'],# NumPy to laod and manipulate datasets in numerical formats
+    'pipe_in': ['stdin', 'stdout', 'stderr'], #sys module
+    'misc': ['imread'], # misc deal with image data
+    'os': ['makedirs'], # os module managing data access 
+    'os.path': ['join', 'exists', 'isdir']
+}
 
-class InputChangeDetector(ast.NodeVisitor):
-    def __init__(self, old_source, new_source):
-        self.old_source = old_source
-        self.new_source = new_source
-        self.changes = []
-        self.detect_changes()
+class CodeChangeDetector(ast.NodeVisitor):
+    def __init__(self, source):
+        self.source = source
+        self.calls = pd.DataFrame(columns=['name', 'node'])
 
-    def detect_changes(self):
-      old_ast = ast.parse(self.old_source)
-      new_ast = ast.parse(self.new_source)
-      self.detectURLChanges(old_ast, new_ast)
-      self.detectListChanges(old_ast, new_ast)
-      self.detectVariableDeletions(old_ast, new_ast)
-      
-      old_functions = {node.name: node for node in ast.walk(old_ast) if isinstance(node, ast.FunctionDef)}
-      new_functions = {node.name: node for node in ast.walk(new_ast) if isinstance(node, ast.FunctionDef)}
+    def find_Call(self, node):
+        for node in ast.walk(node):
+            if isinstance(node, ast.Call):
+                name = ""
+                current = node.func
+                components = []
+                while isinstance(current, ast.Attribute):
+                    components.append(current.attr)
+                    current = current.value
+                if isinstance(current, ast.Name):
+                    components.append(current.id)
+                    components.reverse()
+                    function_name = '.'.join(components)
+                    # Check for os.path specific functions
+                    if function_name.startswith('os.path.'):
+                        sub_function = function_name.split('.')[-1]
+                        if sub_function in input_functions['os.path']:
+                            name = function_name
+                    elif any(function_name.startswith(lib + '.') and function_name.split('.')[1] in funcs for lib, funcs in input_functions.items() if lib != 'os.path'):
+                        name = function_name
+                if name:
+                    new_row = {'name': name, 'node': str(ast.dump(node))}
+                    self.calls.loc[len(self.calls)] = new_row
 
-      for func_name, old_func in old_functions.items():
-        new_func = new_functions.get(func_name)
-        if new_func:
-            self.compare(old_func, new_func)
-        elif 'fps' in old_func.name:  # Example of a specific keyword to identify functions
-            self.changes.append(f"Function related to FPS '{func_name}' was removed.")
+    def visit_Call(self, node):
+        self.find_Call(node)
+        self.generic_visit(node)
 
+def compare(oldcalls, newcalls):
+    diff = difflib.unified_diff(
+        oldcalls["node"].tolist(), newcalls["node"].tolist(), fromfile='file1', tofile='file2', lineterm='', n=0
+    )
+    lines = list(diff)[2:]
+    added = [line[1:] for line in lines if line[0] == '+']
+    removed = [line[1:] for line in lines if line[0] == '-']
+    if added or removed:
+        for linea in added:
+            index = newcalls.loc[newcalls['node'] == linea].index[0]
+            print(f"Added: {newcalls['name'][index]}")
+        for liner in removed:
+            index = oldcalls.loc[oldcalls['node'] == liner].index[0]
+            print(f"Removed: {oldcalls['name'][index]}")
+        return True
+    return False
 
-
-
-    def detectURLChanges(self, old_ast, new_ast):
-         old_urls = {}
-         new_urls = {}
-         for node in ast.walk(old_ast):
-           if isinstance(node, ast.Assign) and isinstance(node.value, ast.Str):
-              for target in node.targets:
-                if isinstance(target, ast.Name):  
-                    old_urls[target.id] = node.value.s
-
-         for node in ast.walk(new_ast):
-           if isinstance(node, ast.Assign) and isinstance(node.value, ast.Str):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    new_urls[target.id] = node.value.s
-
-         for var, old_url in old_urls.items():
-          new_url = new_urls.get(var)
-          if new_url and new_url != old_url:
-            self.changes.append(f"URL change for {var}: from {old_url} to {new_url}")
-    
-    def detectListChanges(self, old_ast, new_ast):
-    # Focus on lists that are likely to contain image data or paths
-      def is_image_related_list(list_name):
-        keywords = ['path', 'file', 'photo', 'picture']
-        return any(keyword in list_name.lower() for keyword in keywords)
-
-      old_lists = self.extract_lists(old_ast)
-      new_lists = self.extract_lists(new_ast)
-
-      for list_var in old_lists:
-        if list_var in new_lists and old_lists[list_var] != new_lists[list_var] and is_image_related_list(list_var):
-            self.changes.append(f"Image-related list change for {list_var}: from {old_lists[list_var]} to {new_lists[list_var]}")
-        elif list_var not in new_lists and is_image_related_list(list_var):
-            self.changes.append(f"Image-related list {list_var} removed in new version")
-
-    def extract_lists(self, ast_node):
-      list_dict = {}
-      for node in ast.walk(ast_node):
-        if isinstance(node, ast.Assign) and isinstance(node.value, ast.List):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    list_contents = [elem.n if isinstance(elem, ast.Num) else elem.s if isinstance(elem, ast.Str) else None for elem in node.value.elts]
-                    list_dict[target.id] = list_contents
-      return list_dict
-
-
-
-    def detectVariableDeletions(self, old_ast, new_ast):
-    # Broaden the criteria to include general processing variables
-      def is_critical_image_processing_var(var_name):
-        # Include both explicit image-related keywords and common processing terms
-        image_keywords = ['path']
-        processing_keywords = ['samples']  
-        return any(keyword in var_name.lower() for keyword in image_keywords + processing_keywords)
-
-      old_vars = {node.id for node in ast.walk(old_ast) if isinstance(node, ast.Name)}
-      new_vars = {node.id for node in ast.walk(new_ast) if isinstance(node, ast.Name)}
-
-      deleted_vars = old_vars - new_vars
-      for var in deleted_vars:
-        if is_critical_image_processing_var(var):
-            self.changes.append(f"Critical variable deleted: {var}")
-
-    def compare(self, old_func, new_func):
-      old_method_calls = self.extract_method_calls(old_func)
-      new_method_calls = self.extract_method_calls(new_func)
-
-      for method in set(old_method_calls.keys()).union(new_method_calls.keys()):
-        old_kwargs = old_method_calls.get(method, {})
-        new_kwargs = new_method_calls.get(method, {})
-        
-        for key in set(old_kwargs.keys()).union(new_kwargs.keys()):
-            old_value = old_kwargs.get(key, 'None')  # Default to 'None' if not present
-            new_value = new_kwargs.get(key, 'None')
-            if old_value != new_value:
-                self.changes.append(f"Change detected in method '{method}' for keyword '{key}': from {old_value} to {new_value}")
-
-    def extract_method_calls(self, func_node):
-      method_calls = {}
-      for node in ast.walk(func_node):
-        if isinstance(node, ast.Call):
-            method_name = ''
-            if isinstance(node.func, ast.Attribute):
-                method_name = node.func.attr
-            elif isinstance(node.func, ast.Name):
-                method_name = node.func.id
-
-            # Specifically focus on np.load calls
-            if method_name == 'load': 
-                kwargs = {}
-                for kw in node.keywords:
-                    kwargs[kw.arg] = astor.to_source(kw.value).strip()
-                method_calls[method_name] = kwargs
-
-      return method_calls
-    
 def input_data(df):
     changes = False
     count = 0
     for index, row in df.iterrows():
         old_content = row["oldFileContent"]
         new_content = row["currentFileContent"]
-        detector = InputChangeDetector(old_content, new_content)
-        detector.detect_changes()
-
-        if detector.changes:
-            print(f"Changes detected in {df['Path'][index]}:")
-            for change in detector.changes:
-                print(f" - {change}")
-            istrue = True
-            if istrue:
-                count = count + 1
+        old_ast = ast.parse(old_content)
+        new_ast = ast.parse(new_content)
+        old_detector = CodeChangeDetector(old_content)
+        new_detector = CodeChangeDetector(new_content)
+        old_detector.find_Call(old_ast)
+        new_detector.find_Call(new_ast)
+        old_calls = old_detector.calls
+        new_calls = new_detector.calls
+        istrue = compare(old_calls, new_calls)
+        if istrue:
+            count += 1
     if count > 0:
         changes = True
-    else:
-        changes = False
-    print(f"Changes detected: {changes}")
+    print(changes)
     return changes
 
 if __name__ == "__main__":
-    repo_path, commit_hash = clone_repo("https://github.com/lengstrom/fast-style-transfer/commit/ffdec65daf2e323f9577f402120270e4c53ccc00")
+    repo_path, commit_hash = clone_repo("https://github.com/lsheiba/CapsNet-Tensorflow-1/commit/7c80553e049ab43aa3726d9353c3c63e71712487")
     df, analyze = commit_changes(repo_path, commit_hash)
     if analyze:
         input_data(df)
